ca0608ad530fcc248542e06556ce1378442a751c
bryopsida <8363252+bryopsida@users.noreply.github.com>
2023-10-01 07:01:20 -0500

    Initial setup

---
+++ test/canary.spec.ts
@@ -0,0 +1,7 @@
+import { describe, it, expect, beforeAll, afterAll } from '@jest/globals'
+
+describe('canary', () => {
+  it('should be able to make assertions', () => {
+    expect(true).toBeTruthy()
+  })
+})
